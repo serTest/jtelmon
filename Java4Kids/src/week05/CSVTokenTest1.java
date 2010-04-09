9ffac4115ffd8070ec139ca8856e52a7588a9d01
auntaru <auntaru@fbeace9d-d74f-0410-b0ac-cf0a61670a25>
2010-04-09 23:52:04 +0000

    qf

--- Java4Kids/src/week05/CSVTokenTest1.java
+++ Java4Kids/src/week05/CSVTokenTest1.java
@@ -3,7 +3,7 @@ package week05;
 
 public class CSVTokenTest1 {
     public static void main(String[] args){
-        CSVTokenizer csvt = new CSVTokenizer("this,is,a,test");
+        CSVTokenizer csvt = new CSVTokenizer(" \"this\" ,1, \"is\" , \"a\" , \"test\" ", ",", "\"");
         while (csvt.hasMoreTokens()) {
             System.out.println(csvt.nextToken());
         }
