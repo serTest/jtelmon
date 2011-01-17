f941958bbd9598ddac1fb90e50e4f1aa01943563
auntaru <auntaru@fbeace9d-d74f-0410-b0ac-cf0a61670a25>
2011-01-17 18:04:38 +0000

    Teste Android JLsoft RunSales

---
+++ trunk/AndroidViewTest1/src/teste/vederi/teste.java
@@ -0,0 +1,19 @@
+package teste.vederi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class teste extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        //---load the BasicViewsExample activity---
+        startActivity(new Intent(this, Vederi.class));
+        
+        
+    }
+}
